294977e41358dedd48c82a85d0f999a540214bfb
Sebastian Budyński <Sebbudynski@gmail.com>
2024-03-11 16:31:08 +0100

    Adding todo in dom

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 "use strict";
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+const input = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const template = document.querySelector("#todo-template");
+
+function updateTodoList() {
+  todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    const newTodo = template.content.cloneNode(true);
+    newTodo.querySelector("span").textContent = todo;
+    todoList.appendChild(newTodo);
+  });
+}
+
+updateTodoList();
+
+document
+  .querySelector("#todo-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    todos.push(input.value);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    const newTodo = template.content.cloneNode(true);
+    newTodo.querySelector("span").textContent = input.value;
+    todoList.appendChild(newTodo);
+    input.value = "";
+  });
+
+// localStorage.clear();
