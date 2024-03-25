9362240e70ed7839e06f123e98520aee10d27929
Sebastian Budyński <Sebbudynski@gmail.com>
2024-03-25 18:51:14 +0100

    add bright mode

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 "use strict";
 
-let todos = JSON.parse(localStorage.getItem("todos")) || [];
 const input = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 const template = document.querySelector("#todo-template");
 const todoForm = document.querySelector("#todo-form");
+const switchBtn = document.querySelector(".switch");
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function updateTodoList() {
   todoList.innerHTML = "";
   todos.forEach((todo, index) => {
     const newTodo = template.content.cloneNode(true);
     newTodo.querySelector("span").textContent = todo.text;
+    const li = newTodo.querySelector("li");
     if (todo.done) {
       newTodo.querySelector(".todo-text").classList.add("line-through");
+      newTodo.querySelector(".status").classList.add("check");
     }
     newTodo.querySelector(".cross").addEventListener("click", function () {
       todos.splice(index, 1);
       localStorage.setItem("todos", JSON.stringify(todos));
-      updateTodoList();
+      li.remove();
     });
-    const li = newTodo.querySelector("li");
     li.addEventListener("click", () => {
       if (typeof todo === "object" && todo !== null) {
         todo.done = !todo.done;
@@ -44,3 +46,20 @@ todoForm.addEventListener("submit", function (event) {
     input.value = "";
   }
 });
+
+function brightMode() {
+  switchBtn.classList.toggle("bright-mode");
+  document.querySelector("body").classList.toggle("bright-mode");
+  document.querySelector("#todo-list").classList.toggle("bright-mode");
+  document.querySelector(".item").classList.toggle("bright-mode");
+  let isBrightMode = document.body.classList.contains("bright-mode");
+  localStorage.setItem("bright-mode", isBrightMode);
+}
+
+switchBtn.addEventListener("click", brightMode);
+
+window.onload = () => {
+  if (localStorage.getItem("bright-mode") === "true") {
+    brightMode();
+  }
+};
